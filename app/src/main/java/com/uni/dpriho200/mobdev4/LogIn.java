d5ebc1a70c4f83de63c95f789a8d45b4648bd72b
Daniel Prihodko <youman6@gmail.com>
2016-11-08 14:59:24 +0000

    second stage of testing - got access to the required data fields and the needed cookie, trying to send a proper request

--- app/src/main/java/com/uni/dpriho200/mobdev4/LogIn.java
+++ app/src/main/java/com/uni/dpriho200/mobdev4/LogIn.java
@@ -1,19 +1,27 @@
 package com.uni.dpriho200.mobdev4;
 
+import android.provider.DocumentsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.CookieHandler;
 import java.net.CookieManager;
+import java.net.CookiePolicy;
 import java.net.HttpCookie;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -37,19 +45,19 @@ public class LogIn extends AppCompatActivity
 
         //tracking the cookies - we need them for ASP.NET session tracking
         CookieManager cookieManager = new CookieManager();
+        cookieManager.setCookiePolicy(CookiePolicy.ACCEPT_ALL);
         CookieHandler.setDefault(cookieManager);
         Thread t = new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
-                    URL url = new URL("https://celcat.gcu.ac.uk/calendar/Login.aspx");
-                    HttpURLConnection conn = (HttpURLConnection)url.openConnection();
-                    try {
-                        InputStream in = new BufferedInputStream(conn.getInputStream());
-
-                    } finally {
-                        conn.disconnect();
-                    }
+                    Document doc = Jsoup.connect("https://celcat.gcu.ac.uk/calendar/Login.aspx").get();
+                    viewState = doc.select("#__VIEWSTATE").first().val();
+                    eventValidation = doc.select("#__EVENTVALIDATION").first().val();
+                    cookiesReady = true;
+                    //Log.i("CW", "ViewState=" + viewState + " | EventValidation" + eventValidation);
+                    //HttpCookie cookie = ((CookieManager)CookieManager.getDefault()).getCookieStore().getCookies().get(0);
+                    //Log.i("CW", cookie.getName() + ": " + cookie.getValue());
                 } catch(Exception e) {
                     Log.e("CW", e.toString());
                 }
@@ -61,43 +69,77 @@ public class LogIn extends AppCompatActivity
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                try {
-                    //safety check
-                    if(!cookiesReady) {
-                        Log.w("CW", "Didn't finish receiving log in data, returning early.");
-                        return;
+                Thread t = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            //safety check
+                            if(!cookiesReady) {
+                                Log.w("CW", "Didn't finish receiving log in data, returning early.");
+                                return;
+                            }
+
+                            //first, forming the post data
+                            String payload = "";
+                            payload += "__VIEWSTATE=" + viewState;
+                            payload += "&__EVENTVALIDATION=" + eventValidation;
+                            payload += "&txtUserName=dpriho200";// + userField.getText();
+                            payload += "&txtUserPass=Ffzkdpcu1";// + paswField.getText();
+                            payload += "&btnLogin=+++Log+In+++";
+
+                            //payload formed, sending
+                            URL url = new URL("https://celcat.gcu.ac.uk/calendar/Login.aspx");
+                            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+                            try {
+                                HttpCookie cookie = ((CookieManager)CookieManager.getDefault()).getCookieStore().getCookies().get(0);
+                                String cookieStr = cookie.getName() + "=" + cookie.getValue();
+
+                                conn.setRequestMethod("POST");
+                                conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+                                conn.setRequestProperty("Cookie", cookieStr);
+                                conn.setDoOutput(true);
+                                conn.setFixedLengthStreamingMode(payload.length());
+
+                                OutputStream out = new BufferedOutputStream(conn.getOutputStream());
+                                out.write(payload.getBytes("UTF-8"));
+                                out.flush();
+                                out.close();
+                                conn.connect();
+                                int responseCode=conn.getResponseCode();
+                                String response = "";
+                                if (responseCode == HttpsURLConnection.HTTP_OK) {
+                                    String line;
+                                    BufferedReader br=new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                                    while ((line=br.readLine()) != null)
+                                        response+=line;
+                                }
+                                Log.i("CW", "Page: (" + responseCode + ") " + response);
+                            } finally {
+                                conn.disconnect();
+                            }
+                        } catch(Exception e) {
+                            Log.e("CW", e.toString());
+                        }
                     }
-
-                    //first, forming the post data
-                    String payload = "";
-                    payload += "__VIEWSTATE=" + viewState;
-                    payload += "&__EVENTVALIDATION=" + eventValidation;
-                    payload += "&txtUserName=" + userField.getText();
-                    payload += "&txtUserPass=" + paswField.getText();
-                    payload += "&btnLogin=+++Log+In+++";
-
-                    //payload formed, sending
-                    URL url = new URL("https://celcat.gcu.ac.uk/calendar/Login.aspx");
-                    HttpURLConnection conn = (HttpURLConnection)url.openConnection();
-                    try {
-                        conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-                        conn.setDoOutput(true);
-                        conn.setFixedLengthStreamingMode(payload.length());
-
-                        OutputStream out = new BufferedOutputStream(conn.getOutputStream());
-
-                    } finally {
-                        conn.disconnect();
-                    }
-                } catch(Exception e) {
-                    Log.e("CW", e.toString());
-                }
+                });
+                t.start();
             }
         });
     }
 
-    static String readStream(BufferedInputStream stream)
-    {
-        return "";
+    static String readStream(BufferedInputStream stream) {
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+        int length;
+        String result = "";
+        try {
+            while ((length = stream.read(buffer)) != -1) {
+                output.write(buffer, 0, length);
+            }
+            result = output.toString("UTF-8");
+        } catch(Exception e) {
+            Log.e("CW", e.toString());
+        }
+        return result;
     }
 }
